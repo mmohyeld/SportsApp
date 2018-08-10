975fa161fdcd46f8b3e0252c736796a66220d8f9
Mohamed Mohyeldin <33186941+mmohyeld@users.noreply.github.com>
2018-08-09 20:39:53 -0400

    Add tabs to team page

--- ManUtd.js
+++ ManUtd.js
@@ -2,8 +2,96 @@ import React from 'react';
 import { StyleSheet, Text, View, Component, TouchableHighlight, Image, SectionList, Platform, ScrollView, StatusBar } from 'react-native';
 import { List, ListItem } from 'react-native-elements';
 import ArsenalPlayers from './ArsenalPlayers';
+import { createStackNavigator, createMaterialTopTabNavigator } from 'react-navigation';
+
+class Squad extends React.Component {
+  render(){
+    const teamColour = this.props.navigation.getParam('colour', '#d81920')
+    const roster = this.props.navigation.getParam('roster', ArsenalPlayers)
+    return (
+      <ScrollView>
+        <View style={styles.listContainer}>
+          <List containerStyle={{marginBottom: 20}}>
+            {
+              roster['manager'].map((l, i) => (
+              <ListItem
+                roundAvatar
+                avatar={{uri:l.avatar_url}}
+                key={i}
+                title={l.name}
+                subtitle={l.subtitle}
+              />
+              ))
+            }
+          </List>
+        </View>
+        <View style={styles.listContainer}>
+          <List containerStyle={{marginBottom: 20}}>
+            {
+              roster['players'].map((l, i) => (
+              <ListItem
+                roundAvatar
+                avatar={{uri:l.avatar_url}}
+                key={i}
+                title={l.name}
+                subtitle={l.subtitle}
+                onPress={() => this.props.navigation.navigate('Player', {name: l.name, 
+                  photo: l.avatar_url, 
+                  personal: l.personal, 
+                  colour: teamColour,
+                  allTime: l.allTimeStats,
+                  pos: l.subtitle
+                })}
+              />
+              ))
+            }
+          </List>
+        </View>
+      </ScrollView>
+    );
+  }
+}
+
+class Fixtures extends React.Component {
+  render(){
+    const teamColour = this.props.navigation.getParam('colour', '#d81920')
+    const roster = this.props.navigation.getParam('roster', ArsenalPlayers)
+    return (
+      <ScrollView>
+        <Text style={styles.title}>Next 5 Fixtures</Text>
+      </ScrollView>
+    );
+  }
+}
+
+class Results extends React.Component {
+  render(){
+    const teamColour = this.props.navigation.getParam('colour', '#d81920')
+    const roster = this.props.navigation.getParam('roster', ArsenalPlayers)
+    return (
+      <ScrollView>
+        <Text style={styles.title}>Last 5 Results</Text>
+      </ScrollView>
+    );
+  }
+}
+
+const TeamStack = createMaterialTopTabNavigator(
+  {
+    Squad: {screen: Squad},
+    Results: {screen: Results},
+    Fixtures: {screen: Fixtures},
+  },
+  {
+    tabBarOptions: {
+      style: {backgroundColor:'#38003C'},
+    },
+  },
+  
+);
 
 export default class ManUtd extends React.Component {
+  static router = TeamStack.router;
   render() {
     const {navigation} = this.props;
     const teamName = navigation.getParam('name', 'default');
@@ -21,45 +109,7 @@ export default class ManUtd extends React.Component {
             <Text style={[styles.teamName, {color: teamColour == '#f5f5f5' || teamColour == '#fe0' ? '#333': '#fff'}]}>{teamName}</Text>
           </View>
         </View>
-        <ScrollView>
-          <View style={styles.listContainer}>
-            <List containerStyle={{marginBottom: 20}}>
-              {
-                roster['manager'].map((l, i) => (
-                <ListItem
-                  roundAvatar
-                  avatar={{uri:l.avatar_url}}
-                  key={i}
-                  title={l.name}
-                  subtitle={l.subtitle}
-                />
-                ))
-              }
-            </List>
-          </View>
-          <View style={styles.listContainer}>
-            <List containerStyle={{marginBottom: 20}}>
-              {
-                roster['players'].map((l, i) => (
-                <ListItem
-                  roundAvatar
-                  avatar={{uri:l.avatar_url}}
-                  key={i}
-                  title={l.name}
-                  subtitle={l.subtitle}
-                  onPress={() => this.props.navigation.navigate('Player', {name: l.name, 
-                    photo: l.avatar_url, 
-                    personal: l.personal, 
-                    colour: teamColour,
-                    allTime: l.allTimeStats,
-                    pos: l.subtitle
-                  })}
-                />
-                ))
-              }
-            </List>
-          </View>
-        </ScrollView>
+        <TeamStack navigation={this.props.navigation} />
       </View>
     );
   }
